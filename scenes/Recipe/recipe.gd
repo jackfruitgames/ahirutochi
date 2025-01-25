e15b26cf1d87c8c92a983edd99490d6372ddd82a
Jan Bucher <jan@bucher.cloud>
2025-01-25 22:38:21 +0100

    Show checkmarks in recipe

--- scenes/Recipe/recipe.gd
+++ scenes/Recipe/recipe.gd
@@ -6,13 +6,23 @@ var textures: Array = [
 	preload("res://assets/img/ingredient/BLUE.png"),
 ]
 
-
-func _ready() -> void:
+func _process(delta: float) -> void:
 	set_ingredients()
-
 
 func set_ingredients() -> void:
 	var ingredients = GameState.current_recipe
 	$Ingredient1.texture = textures[ingredients[0]]
 	$Ingredient2.texture = textures[ingredients[1]]
 	$Ingredient3.texture = textures[ingredients[2]]
+	if len(GameState.current_used_ingredients) >= 1 and GameState.current_used_ingredients[0] == GameState.current_recipe[0]:
+		$Ingredient1_checkmark.visible = true
+	else:
+		$Ingredient1_checkmark.visible = false
+	if len(GameState.current_used_ingredients) >= 2 and GameState.current_used_ingredients[1] == GameState.current_recipe[1]:
+		$Ingredient2_checkmark.visible = true
+	else:
+		$Ingredient2_checkmark.visible = false
+	if len(GameState.current_used_ingredients) >= 3 and GameState.current_used_ingredients[2] == GameState.current_recipe[2]:
+		$Ingredient3_checkmark.visible = true
+	else:
+		$Ingredient3_checkmark.visible = false
