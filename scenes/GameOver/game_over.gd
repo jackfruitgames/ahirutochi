e35fb4f9313eac728f6b1e407e845758d2ea6904
eddex <eddex@outlook.com>
2025-01-25 19:26:25 +0100

    beautiful game over screen

--- scenes/GameOver/game_over.gd
+++ scenes/GameOver/game_over.gd
@@ -1,6 +1,12 @@
-extends Node2D
+extends Control
 
 func _on_restart_button_pressed() -> void:
 	GameState.reset_state()
 	var gameScene: PackedScene = load("res://scenes/Game/game.tscn")
+	get_tree().change_scene_to_packed(gameScene)
+
+
+func _on_main_menu_button_pressed() -> void:
+	GameState.reset_state()
+	var gameScene: PackedScene = load("res://scenes/MainMenu/main_menu.tscn")
 	get_tree().change_scene_to_packed(gameScene)
