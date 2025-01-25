f7deeef1cc5f6bf700795ad30bfbfbdfc7d82ee3
Jan Bucher <jan@bucher.cloud>
2025-01-25 13:20:09 +0100

    Colorize health bar

--- scenes/House/house.gd
+++ scenes/House/house.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var RED: Color = Color.html("#eb4034")
+var GREEN: Color = Color.html("#67b000")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,4 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	get_node("HealthBar").value = GameState.health
+	var new_health: int = GameState.health
+	var style = $HealthBar.get("theme_override_styles/fill")
+	if new_health < 30:
+		style.bg_color = RED
+	else:
+		style.bg_color = GREEN
+	$HealthBar.value = new_health
