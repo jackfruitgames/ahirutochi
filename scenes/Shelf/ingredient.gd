6454b2964fa87ad2a3d0d06cd24ab08eb9cd7243
Benj Fassbind <randombenj@gmail.com>
2025-01-25 15:26:01 +0100

    Use ingredient in pot

--- scenes/Shelf/ingredient.gd
+++ scenes/Shelf/ingredient.gd
@@ -12,6 +12,7 @@ func _process(delta: float) -> void:
 var ingredient: Enums.INGREDIENT
 var selected = false
 var start_position = Vector2()
+var pot_position
 
 func set_type(type: Enums.INGREDIENT):
 	var sprite = $Ingredient
@@ -26,29 +27,39 @@ func reset_position():
 	position = start_position
 
 func _on_ingredient_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if GameState.is_hand_full:
-		return 
-		
 	if Input.is_action_just_pressed("ClickOnShelfItem"):
 		GameState.is_hand_full = true
 		selected = true
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
+	if selected and event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
 			selected = false
 			GameState.is_hand_full = false
+			use()
 
 func _physics_process(delta: float) -> void:
 	if selected:
 		global_position = lerp(global_position, get_global_mouse_position(), 25 * delta)
+		
+	if pot_position != null:
+		global_position = lerp(global_position, pot_position, 25 * delta)
+		
+		if global_position.is_equal_approx(pot_position):
+			use()
 
 func _on_ingredient_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "PotContentArea":
 		selected = false
 		GameState.is_hand_full = false
 		
-		reset_position()
+		var pot = area.get_parent()
+		pot_position = pot.global_position
+		#reset_position()
 	
-		area.get_parent().add_ingredient(ingredient)
-	print("COLLISION", area)
+		pot.add_ingredient(ingredient)
+	
+func use():
+	"""
+	Uses the current ingredient and destroys it
+	"""
+	queue_free()
