8e0f053076089b80b0212fdcec085490be93c242
Jan Bucher <jan@bucher.cloud>
2025-01-25 14:50:41 +0100

    Implement filling the soda dream tank

--- scenes/SodaDream/soda_dream.gd
+++ scenes/SodaDream/soda_dream.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 signal shot_fired
-
+var tank_fill_procentage = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +14,19 @@ func _process(delta: float) -> void:
 
 
 func _on_tmp_hit_button_pressed() -> void:
+	tank_fill_procentage = 0
+	$TankFillProgressBar.value = tank_fill_procentage
 	shot_fired.emit()
+
+
+func _on_tmp_fill_tank_button_pressed() -> void:
+	$TankFillTimer.start()
+
+
+func _on_tank_fill_timer_timeout() -> void:
+	if tank_fill_procentage < 100:
+		tank_fill_procentage += 10
+	else:
+		$TankFillTimer.stop()
+
+	$TankFillProgressBar.value = tank_fill_procentage
