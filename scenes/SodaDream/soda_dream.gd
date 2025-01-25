3639a3de65bc1de5bb005a7678580653cc0ac681
Jan Bucher <jan@bucher.cloud>
2025-01-25 15:19:15 +0100

    Implement preasurizing logic

--- scenes/SodaDream/soda_dream.gd
+++ scenes/SodaDream/soda_dream.gd
@@ -1,32 +1,48 @@
 extends Node2D
 
 signal shot_fired
+# Fill of the tank from 0 to 100
 var tank_fill_procentage = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Pressure from 0 to 180 max
+var pressure = 0
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# start the whole filling process from outside
+func start_filling() -> void:
+	$TankFillTimer.start()
 
 
 func _on_tmp_hit_button_pressed() -> void:
-	tank_fill_procentage = 0
-	$TankFillProgressBar.value = tank_fill_procentage
-	shot_fired.emit()
+	if pressure >= 180:
+		$PressureTimer.stop()
+		tank_fill_procentage = 0
+		$TankFillProgressBar.value = tank_fill_procentage
+		pressure = 0
+		$PressureGaugeNumber.text = str(pressure)
+		$TmpFillTankButton.disabled = false
+		shot_fired.emit()
 
 
 func _on_tmp_fill_tank_button_pressed() -> void:
-	$TankFillTimer.start()
+	start_filling()
+	$TmpFillTankButton.disabled = true
 
 
 func _on_tank_fill_timer_timeout() -> void:
 	if tank_fill_procentage < 100:
 		tank_fill_procentage += 10
 	else:
+		# Stop the filling, start the pressurizing
 		$TankFillTimer.stop()
+		$PressureTimer.start()
 
 	$TankFillProgressBar.value = tank_fill_procentage
+
+
+func _on_pressure_timer_timeout() -> void:
+	if pressure < 180:
+		pressure += 10
+		$PressureGaugeNumber.text = str(pressure)
+	else:
+		# pressure at max
+		pass
