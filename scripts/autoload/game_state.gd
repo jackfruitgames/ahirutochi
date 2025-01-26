a2e9c9eaa3559adec49553939b971698a022bb09
eddex <eddex@outlook.com>
2025-01-26 11:16:28 +0100

    add score to game state

--- scripts/autoload/game_state.gd
+++ scripts/autoload/game_state.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var score: int = 0
 var health = 100
 var is_hand_full = false
 var current_recipe: Array = [
@@ -11,6 +12,7 @@ var current_used_ingredients: Array = []
 var current_pot_color: Color = Color.BLACK
 
 func reset_state() -> void:
+	score = 0
 	health = 100
 	is_hand_full = false
 	current_recipe = [
